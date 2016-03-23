8fc7ade62e497f6102e4f1708cbd259a49739cee
Bryan <bwbh87@gmail.com>
2016-03-24 00:21:59 +0800

    Added method for Fixed and Prestige computation via Mechanize gem

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'devise'
 gem 'rack-cors', require: "rack/cors"
 gem 'active_model_serializers'
 gem 'friendly_id'
+gem 'mechanize'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/model_type.rb
+++ app/models/model_type.rb
@@ -1,8 +1,22 @@
+require 'mechanize'
+
 class ModelType < ActiveRecord::Base
   validates	:name, :model_type_slug, :model_type_code, :base_price, :model_id, presence: true
 
 	belongs_to :model
 
 	def total_price
+		if self.model.organization.pricing_policy == "Flexible"
+		elsif self.model.organization.pricing_policy == "Fixed"
+			mechanize = Mechanize.new
+			page = mechanize.get('https://developer.github.com/v3/#http-redirects')
+			return total = self.base_price + margin.search("//*[contains(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'),'status')]").count
+
+		else self.model.organization.pricing_policy == "Prestige"
+			mechanize = Mechanize.new
+			margin = mechanize.get('http://www.yourlocalguardian.co.uk/sport/rugby/rss/')
+
+			return total = self.base_price + margin.search('pubDate').count
+		end
 	end
 end
