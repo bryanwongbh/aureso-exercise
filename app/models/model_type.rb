9cbc8863e7accecc0040ec29ef96889e598ca545
Bryan Wong <bwbh87@gmail.com>
2016-03-23 03:37:14 +0000

    Added presence validation for model_id in ModelType model

--- app/models/model_type.rb
+++ app/models/model_type.rb
@@ -1,5 +1,5 @@
 class ModelType < ActiveRecord::Base
-  validates	:name, :model_type_slug, :model_type_code, :base_price, presence: true
+  validates	:name, :model_type_slug, :model_type_code, :base_price, :model_id, presence: true
 
 	belongs_to :model
 end
--- spec/models/model_type_spec.rb
+++ spec/models/model_type_spec.rb
@@ -6,6 +6,7 @@ describe ModelType do
 		it { should validate_presence_of(:model_type_slug)}
 		it { should validate_presence_of(:model_type_code)}
 		it { should validate_presence_of(:base_price)}
+		it { should validate_presence_of(:model_id)}
 	end
 	context "relationships" do
 		it { should belong_to(:model)}
