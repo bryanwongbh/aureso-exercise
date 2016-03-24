8802ea413ee1a966e4cc6f2ead81e77e8a16957e
Bryan Wong <bwbh87@gmail.com>
2016-03-24 10:36:36 +0000

    Added algorithm to compute for Flexible pricing_policy

--- app/models/model_type.rb
+++ app/models/model_type.rb
@@ -1,4 +1,6 @@
 require 'mechanize'
+require 'nokogiri'
+require 'open-uri'
 
 class ModelType < ActiveRecord::Base
   validates	:base_price, :model_id, presence: true
@@ -12,16 +14,34 @@ class ModelType < ActiveRecord::Base
 
 	def total_price
 		if self.model.organization.pricing_policy == "Flexible"
+		  
+		  page = Nokogiri::HTML(open("http://reuters.com"))
+		  
+      node_array = []
+      margin = 0
+      
+      page.traverse{ |x|
+          if x.text? and not x.text =~ /^\s*$/
+              node_array << x
+          end
+      }
+      node_array.each do |i|
+          margin = margin + i.to_s.count('a')
+      end
+      return total = self.base_price * margin/100
+      
 		elsif self.model.organization.pricing_policy == "Fixed"
+		
 			mechanize = Mechanize.new
 			margin = mechanize.get('https://developer.github.com/v3/#http-redirects')
 			return total = self.base_price + margin.search("//*[contains(translate(text(),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'),'status')]").count
 
 		else self.model.organization.pricing_policy == "Prestige"
+		  
 			mechanize = Mechanize.new
 			margin = mechanize.get('http://www.yourlocalguardian.co.uk/sport/rugby/rss/')
-
 			return total = self.base_price + margin.search('pubDate').count
+			
 		end
 	end
 
